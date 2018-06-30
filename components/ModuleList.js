7285b733f8bb8c9062a26c13dc2b11bd63edba9b
Dhruv <dhruvdev7794@gmail.com>
2018-06-30 13:27:06 -0400

    added list to moduleList

--- components/ModuleList.js
+++ components/ModuleList.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import {Text} from 'react-native-elements'
+import {View} from 'react-native';
+import {Text, ListItem} from 'react-native-elements'
 
 class ModuleList extends React.Component{
+
+    static navigationOptions = {title: 'Modules'};
+    constructor(props){
+        super(props);
+        this.state={
+            modules:[],
+            courseId:1
+        }
+    }
+
+    componentDidMount(){
+        const courseId = this.props.navigation.getParam("courseId", 1);
+        this.setState({
+            courseId: courseId
+        });
+        fetch('http://localhost:8080/api/course/' + courseId + '/module')
+            .then(response => (response.json()))
+            .then(modules => this.setState({modules: modules}))
+
+    }
     render(){
         return(
-            <Text h2>Module List</Text>
+            <View>
+                <Text h2>Module List</Text>
+                {this.state.modules.map((module, index) => (
+                    <ListItem
+                        key={index}
+                        title={module.title}
+                    />
+                ))}
+            </View>
         )
     }
 }
